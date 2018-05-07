0a40d2906c4c9a46efb5cc15f774c7752ad05204
Vijayakumar Mariappan <vijayakumar.mariappan@syncfusion.com>
2018-05-07 15:59:46 +0530

    LaunchScreen images are included.

---
+++ ExpenseAnalysis/ExpenseAnalysis.iOS/LaunchScreenViewController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UIKit;
+
+namespace SampleBrowser
+{
+    public partial class LaunchScreenViewController : UIViewController
+    {
+        static bool UserInterfaceIdiomIsPhone
+        {
+            get { return UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Phone; }
+        }
+
+        public LaunchScreenViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+        }
+
+        #region View lifecycle
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+        }
+
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+        }
+
+        public override void ViewWillDisappear(bool animated)
+        {
+            base.ViewWillDisappear(animated);
+        }
+
+        public override void ViewDidDisappear(bool animated)
+        {
+            base.ViewDidDisappear(animated);
+        }
+
+        #endregion
+    }
+}
