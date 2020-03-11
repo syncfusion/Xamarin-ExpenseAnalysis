31da3fde2d94cbd3aabb843ff83e3abdbf7fbaee
RathanakumarSekar <36629214+RathanakumarSekar@users.noreply.github.com>
2020-03-11 17:23:19 +0530

    Compilation issue fixed committed. (#19)
    * NuGet update changes committed.
    
    * Project json updated.

--- ExpenseAnalysis/ExpenseAnalysis/Model/AddTransactionDetail.cs
+++ ExpenseAnalysis/ExpenseAnalysis/Model/AddTransactionDetail.cs
@@ -1,6 +1,7 @@
 ﻿using Syncfusion.XForms.DataForm;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
